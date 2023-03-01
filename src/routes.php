6ef138eaa87ea66bd5a29ce2fbfeb9d964db2b4c
Farindra Eka Putra <farindra.project@gmail.com>
2023-03-01 11:47:08 +0700

    first commit

--- src/routes.php
+++ src/routes.php
@@ -3,10 +3,10 @@
 Route::group(['middleware' => config('menu.middleware')], function () {
     //Route::get('wmenuindex', array('uses'=>'\Farindra\Menugenerator\Controllers\MenuController@wmenuindex'));
     $path = rtrim(config('menu.route_path'));
-    Route::post($path . '/addcustommenu', array('uses' => '\Farindra\Menugenerator\Controllers\MenuController@addcustommenu'));
-    Route::post($path . '/deleteitemmenu', array('uses' => '\Farindra\Menugenerator\Controllers\MenuController@deleteitemmenu'));
-    Route::post($path . '/deletemenug', array('uses' => '\Farindra\Menugenerator\Controllers\MenuController@deletemenug'));
-    Route::post($path . '/createnewmenu', array('uses' => '\Farindra\Menugenerator\Controllers\MenuController@createnewmenu'));
-    Route::post($path . '/generatemenucontrol', array('uses' => '\Farindra\Menugenerator\Controllers\MenuController@generatemenucontrol'));
-    Route::post($path . '/updateitem', array('uses' => '\Farindra\Menugenerator\Controllers\MenuController@updateitem'));
+    Route::post($path . '/addcustommenu', array('as' => 'addcustommenu', 'uses' => '\Farindra\Menugenerator\Controllers\MenuController@addcustommenu'));
+    Route::post($path . '/deleteitemmenu', array('as' => 'deleteitemmenu', 'uses' => '\Farindra\Menugenerator\Controllers\MenuController@deleteitemmenu'));
+    Route::post($path . '/deletemenug', array('as' => 'deletemenug', 'uses' => '\Farindra\Menugenerator\Controllers\MenuController@deletemenug'));
+    Route::post($path . '/createnewmenu', array('as' => 'createnewmenu', 'uses' => '\Farindra\Menugenerator\Controllers\MenuController@createnewmenu'));
+    Route::post($path . '/generatemenucontrol', array('as' => 'generatemenucontrol', 'uses' => '\Farindra\Menugenerator\Controllers\MenuController@generatemenucontrol'));
+    Route::post($path . '/updateitem', array('as' => 'updateitem', 'uses' => '\Farindra\Menugenerator\Controllers\MenuController@updateitem'));
 });
